0833c909603c7010ad12303caba05aefdb510c80
Mark Vadeika <markvadeika@gmail.com>
2017-09-10 00:11:39 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by AllTheMegahertz on 9/10/2017.
+ */
+
+public class TestOpMode extends OpMode {
+
+
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void loop() {
+
+	}
+}
