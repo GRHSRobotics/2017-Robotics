339cde8ada56007f54384b9c1d32f4fce8cea69b
Mark Vadeika <markvadeika@gmail.com>
2017-10-25 15:49:22 -0400

    Initial Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by AllTheMegahertz on 9/10/2017.
@@ -12,8 +14,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @Autonomous(name="Basic Debug", group="debug")
 public class TestOpMode extends OpMode {
 
+	private Servo servo;
+
 	@Override
 	public void init() {
+
+		servo = hardwareMap.servo.get("linearServo");
 
 	}
 
@@ -21,6 +27,7 @@ public class TestOpMode extends OpMode {
 	public void loop() {
 
 		telemetry.addData("time", getRuntime());
+		telemetry.addData("servoPosition", servo.getPosition());
 		telemetry.update();
 
 	}
