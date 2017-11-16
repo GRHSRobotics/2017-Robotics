b1448dfba070477f9c4538386b3a22d847d66688
Mark Vadeika <markvadeika@gmail.com>
2017-11-16 15:28:47 -0500

    Added strafe support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorDebug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorDebug.java
@@ -2,11 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-import java.util.ArrayList;
-
-@Autonomous(name = "motorDebug", group = "debug")
+@TeleOp(name = "motorDebug", group = "debug")
 public class MotorDebug extends OpMode {
 
 	private DcMotor motorFrontLeft;
@@ -14,14 +14,21 @@ public class MotorDebug extends OpMode {
 	private DcMotor motorBackLeft;
 	private DcMotor motorBackRight;
 	private DcMotor motorArm;
+	private Servo servo0;
+	private Servo servo1;
 
 	@Override
 	public void init() {
+
 		motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
 		motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
 		motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
 		motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
 		motorArm = hardwareMap.dcMotor.get("motorArm");
+
+		servo0 = hardwareMap.servo.get("servo0");
+		servo1 = hardwareMap.servo.get("servo1");
+
 	}
 
 	@Override
@@ -32,10 +39,32 @@ public class MotorDebug extends OpMode {
 		motorBackLeft.setPower(clamp(gamepad1.left_stick_y - gamepad1.left_stick_x));
 		motorBackRight.setPower(clamp(-gamepad1.right_stick_y - gamepad1.right_stick_x));
 
-		motorArm.setPower(clamp(gamepad1.right_trigger - gamepad1.left_trigger));
+		motorArm.setPower(clamp(gamepad1.right_trigger + gamepad2.right_trigger - gamepad1.left_trigger - gamepad2.left_trigger));
+
+		if (gamepad1.left_bumper || gamepad2.left_bumper) {
+			servo0.setPosition(0.3);
+			servo1.setPosition(0.3);
+		} else if (gamepad1.right_bumper || gamepad2.right_bumper) {
+			servo0.setPosition(0.725);
+			servo1.setPosition(0.725);
+		}
+
+		telemetry.addData("servo0", servo0.getPosition());
+		telemetry.addData("servo1", servo1.getPosition());
+		telemetry.addData("arm", motorArm.getCurrentPosition());
+		telemetry.addData("time", getRuntime());
 
 		telemetry.update();
 		
+	}
+
+	@Override
+	public void stop() {
+		motorBackLeft.setPower(0);
+		motorBackRight.setPower(0);
+		motorFrontLeft.setPower(0);
+		motorFrontRight.setPower(0);
+		super.stop();
 	}
 
 	private float clamp(float f) {
